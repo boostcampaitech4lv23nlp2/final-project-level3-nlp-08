21e383dc0768845258eb253278a9df07ed84f872
siryuon <siryuon@gmail.com>
2023-01-18 09:18:52 +0000

    refactor: add port (apply aistage server)

--- chat_app_new/main.py
+++ chat_app_new/main.py
@@ -121,4 +121,4 @@ async def stack_message(data, collection):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0")
+    uvicorn.run(app, host="0.0.0.0", port=30001)
